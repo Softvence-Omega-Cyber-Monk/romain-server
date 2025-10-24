d33b73c3f0b2ab813a8068ec6575c5493092b562
shalauddinahmedshipon <sshimi910@gmail.com>
2025-10-24 15:37:30 +0600

    student login api done

--- src/module/auth/auth.service.ts
+++ src/module/auth/auth.service.ts
@@ -18,6 +18,8 @@ import {
 } from './dto/forget-reset-password.dto';
 import { ChangePasswordDto } from './dto/change-password.dto';
 import { generateOtpCode, getTokens, hashOtpCode, verifyOtp } from './auth.utils';
+import { StudentLoginDto } from './dto/student-login.dto';
+import { SystemRole } from '@prisma/client';
 
 
 @Injectable()
@@ -93,6 +95,10 @@ export class AuthService {
       throw new ForbiddenException('Invalid credentials');
     }
 
+    if (user.role == SystemRole.STUDENT) {
+      throw new ForbiddenException('Invalid login method for your account type.');
+    }
+
     if (!user.isActive) {
       throw new ForbiddenException('Your account is not Active yet!');
     }
@@ -112,6 +118,58 @@ export class AuthService {
     return { user, ...tokens };
   }
 
+
+  async studentLogin(dto: StudentLoginDto) {
+        
+        const studentProfile = await this.prisma.student.findUnique({
+            where: { registrationNumber: dto.studentId },
+            include: { User: true }
+        });
+
+        if (!studentProfile || !studentProfile.User) {
+            throw new ForbiddenException('Invalid credentials (Student ID not found)');
+        }
+
+        const user = studentProfile.User;
+
+        if (user.role !== SystemRole.STUDENT) {
+             throw new ForbiddenException('Invalid login method for your account type.');
+        }
+
+        if (!user.password) {
+
+            throw new ForbiddenException('Invalid credentials (No password set)');
+        }
+        
+
+        if (!user.isActive) {
+            throw new ForbiddenException('Your account is not Active yet! Please complete activation.');
+        }
+
+        if (user.isDeleted) {
+            throw new BadRequestException('User account is deleted!');
+        }
+
+
+        const isMatch = await bcrypt.compare(dto.password, user.password);
+        if (!isMatch) {
+            throw new ForbiddenException('Invalid credentials');
+        }
+        
+
+        const tokens = await getTokens(
+            this.jwtService,
+            user.id, 
+            user.institutionId || '', 
+            user.email, 
+            user.role
+        );
+
+
+        const { password, ...userWithoutPassword } = user;
+        
+        return { user: userWithoutPassword, ...tokens };
+    }
 
 // refresh token 
   // async refreshTokens(token: string) {
